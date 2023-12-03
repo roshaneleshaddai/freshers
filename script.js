b544113f27ce235221ce9d7fa7e9a285cbdf4db7
dameraharika <147978157+dameraharika@users.noreply.github.com>
2023-12-03 12:11:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
-// script.js
-
 document.addEventListener("DOMContentLoaded", function () {
-    // Set the date we're counting down to
     const countDownDate = new Date("Dec 7, 2023 09:00:00").getTime();
 
-    // Update the countdown every 1 second
     const x = setInterval(function () {
-        // Get the current date and time
         const now = new Date().getTime();
 
-        // Calculate the remaining time
         const distance = countDownDate - now;
 
-        // Calculate days, hours, minutes, and seconds
         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
         const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Display the countdown
         document.getElementById("days").innerText = days < 10 ? "0" + days : days;
         document.getElementById("hours").innerText = hours < 10 ? "0" + hours : hours;
         document.getElementById("minutes").innerText = minutes < 10 ? "0" + minutes : minutes;
         document.getElementById("seconds").innerText = seconds < 10 ? "0" + seconds : seconds;
 
-        // If the countdown is over, display a message
         if (distance < 0) {
             clearInterval(x);
             document.getElementById("timer").innerHTML = "EXPIRED";
@@ -32,5 +23,3 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 1000);
 });
 
-// Placeholder for social icons functionality
-// Add your social icons functionality code here if needed
